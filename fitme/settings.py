75da92244349369e111c3b3e22a2ca1b60248dbd
ricardo.andre.ifc@gmail.com <ricardo.andre.ifc@gmail.com>
2022-09-29 16:04:01 -0300

    mysqlclient

--- fitme/settings.py
+++ fitme/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'fitme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fitme',
+        'USER': 'aluno',
+        'PASSWORD':'aluno',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
