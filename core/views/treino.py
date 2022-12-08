fd72e4dae5d6b7e4bd1a7fd1273ec50ce896703b
Fitme <fitmepi@gmail.com>
2022-12-08 13:17:01 -0300

    retornando somente os dados do usuário

--- core/views/treino.py
+++ core/views/treino.py
@@ -7,6 +7,10 @@ from core.serializers import TreinoDetailSerializer, TreinoSerializer
 class TreinoViewSet(ModelViewSet):
     queryset = Treino.objects.all()
 
+    def get_queryset(self):
+        usuario = self.request.user.id
+        return Treino.objects.filter(usuario=usuario)
+
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
             return TreinoDetailSerializer
